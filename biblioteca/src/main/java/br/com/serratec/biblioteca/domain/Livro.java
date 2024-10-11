3713c6dde7a4b64fc148207368bd7695c8f4e134
RhayPradoF <rhayssa.c.prado@gmail.com>
2024-10-11 17:17:16 -0300

    Correções de erros de execução

--- biblioteca/src/main/java/br/com/serratec/biblioteca/domain/Livro.java
+++ biblioteca/src/main/java/br/com/serratec/biblioteca/domain/Livro.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -30,6 +31,7 @@ public class Livro {
 	@NotBlank(message="Preencha a quantidade de Páginas!")
 	private String qtdPaginas;
 	
+	@Valid
 	@Embedded
 	private Publicacao publicacao;
 	
--- biblioteca/src/main/java/br/com/serratec/biblioteca/exception/ControllerExceptionHandler.java
+++ biblioteca/src/main/java/br/com/serratec/biblioteca/exception/ControllerExceptionHandler.java
@@ -10,9 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Override
